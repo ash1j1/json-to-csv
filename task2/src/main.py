1a54dbc3a952134732a43e73ec1dd326396afcff
Ash <py@test.com>
2024-06-25 06:10:54 +0000

    remove redundant lines of code and use scadir instead of listdir

--- task2/src/main.py
+++ task2/src/main.py
@@ -4,15 +4,9 @@ import os
 import pandas as pd
 import time
 
-def json_to_csv(json_dir, csv_dir):
-    for filename in os.listdir(json_dir):
-        if filename.endswith(".json"):
-            json_file = os.path.join(json_dir, filename)
-            csv_file = os.path.join(csv_dir, filename)
+def json_to_csv(json_file, csv_file):
             # Reads the JSON file and converts it Pandas DataFrames
             df = pd.read_json(json_file, lines=True, encoding='utf-8')
-            csv_file = os.path.join(csv_dir, filename.replace('.json', '.csv'))
-
             # DataFrames to CSV file
             df.to_csv(csv_file, index=False)
 
@@ -20,20 +14,18 @@ def monitor_json_dir(json_dir, csv_dir):
     watched_files = set()
 
     while True:
-        new_files = [f for f in os.listdir(json_dir) if f.endswith(".json") and f not in watched_files]
-
-        for filename in new_files:
-            try:
-                json_file = os.path.join(json_dir, filename)
-                df = pd.read_json(json_file, lines=True, encoding='utf-8')
-                csv_file = os.path.join(csv_dir, filename.replace('.json', '.csv'))
-                df.to_csv(csv_file, index=False)
-
-                watched_files.add(filename)
-                print(f"Converted {filename} to CSV")
-            except Exception as e:
-                print(f"Error converting {filename}: {str(e)}")
-        time.sleep(120)
+        with os.scandir(json_dir) as entries:
+            for entry in entries:
+                if entry.is_file() and entry.name.endswith(".json") and entry.name not in watched_files:
+                    json_file = os.path.join(json_dir, entry.name)
+                    csv_file = os.path.join(csv_dir, entry.name.replace('.json', '.csv'))
+                    try:
+                        json_to_csv(json_file, csv_file)
+                        watched_files.add(entry.name)
+                        print(f"Converted {entry.name} to CSV")
+                    except Exception as e:
+                        print(f"Error converting {entry.name}: {str(e)}")
+        time.sleep(20)
                 
 if __name__ == "__main__":
     print('=' * 20 + ' JSON to CSV converter ' + '=' * 20)
@@ -45,7 +37,7 @@ if __name__ == "__main__":
     os.makedirs(csv_dir, exist_ok=True)
 
     # Convert existing JSON files
-    json_to_csv(json_dir, csv_dir)
+    # json_to_csv(json_dir, csv_dir)
 
     # Monitor the JSON directory for new JSON files to be converted
     monitor_json_dir(json_dir, csv_dir)
