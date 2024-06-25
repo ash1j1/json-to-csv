d5f0563ec293cb1f977f43c25d6e4ee6bb0110e4
Ash <py@test.com>
2024-06-25 05:01:21 +0000

    working script

--- task2/src/main.py
+++ task2/src/main.py
@@ -5,7 +5,6 @@ import pandas as pd
 import time
 
 def json_to_csv(json_dir, csv_dir):
-    # os.chdir(json_dir) 
     for filename in os.listdir(json_dir):
         if filename.endswith(".json"):
             json_file = os.path.join(json_dir, filename)
@@ -21,16 +20,20 @@ def monitor_json_dir(json_dir, csv_dir):
     watched_files = set()
 
     while True:
-        for filename in os.listdir(json_dir):
-            if filename.endswith(".json") and filename not in watched_files:
+        new_files = [f for f in os.listdir(json_dir) if f.endswith(".json") and f not in watched_files]
+
+        for filename in new_files:
+            try:
                 json_file = os.path.join(json_dir, filename)
-                csv_file = os.path.join(csv_dir, filename)
                 df = pd.read_json(json_file, lines=True, encoding='utf-8')
                 csv_file = os.path.join(csv_dir, filename.replace('.json', '.csv'))
                 df.to_csv(csv_file, index=False)
 
                 watched_files.add(filename)
-    time.sleep(10)
+                print(f"Converted {filename} to CSV")
+            except Exception as e:
+                print(f"Error converting {filename}: {str(e)}")
+        time.sleep(120)
                 
 if __name__ == "__main__":
     print('=' * 20 + ' JSON to CSV converter ' + '=' * 20)
